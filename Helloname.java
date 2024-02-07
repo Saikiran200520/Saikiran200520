9ef8069eafa131fbe377638146b6615c4acbbc0d
Saikiran200520 <159221675+Saikiran200520@users.noreply.github.com>
2024-02-07 09:58:56 +0530

    Update Helloname.java

--- Helloname.java
+++ Helloname.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Helloname {
+class Helloname {
 	public static void main(String[] args) {
 		System.out.println("Please enter your name:");
 		Scanner input = new Scanner(System.in);
