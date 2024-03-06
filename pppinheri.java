69efd33f92a15b9c8d0ec54e8cb8bf34d1c4a794
Saikiran200520 <159221675+Saikiran200520@users.noreply.github.com>
2024-03-06 21:42:51 +0530

    Create pppinheri.java

---
+++ pppinheri.java
@@ -0,0 +1,40 @@
+class Public
+{
+    public
+        int publicVar=1;
+
+    private
+        int privateVar=2;
+    
+    protected
+        int protectedVar=3;
+
+    public
+        int getPrivate()
+        {
+            return privateVar;
+        }
+}
+
+/*private class Private
+{
+}*/
+//We can't take a top-level class as private because it would be completely useless as nothing would have access to it.
+
+/*protected class Protected
+{
+}*/
+//We can't take a top-level class as protected because it would be completely useless.
+
+public class InheritanceAccess
+{
+    public static void main(String[] args)
+    {
+        Public pub=new Public();
+        System.out.print("Public : "+pub.publicVar+"\n");
+        //System.out.print("Public : "+pub.privateVar);
+        //We cannot access the privateVar because it is private i.e, accessible only within the class.
+        System.out.print("Private : "+pub.getPrivate()+"\n");
+        System.out.print("Protected : "+pub.protectedVar+"\n");
+    }
+}
